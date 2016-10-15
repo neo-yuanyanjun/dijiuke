0562e88de19e57f106643299dedde1a76c994f96
neo-home-win7 <yyjpig@aliyun.com>
2016-10-16 02:20:16 +0800

    add banner form

--- admin/client/component/mainpage/Banners/AddBanner.js
+++ admin/client/component/mainpage/Banners/AddBanner.js
@@ -3,8 +3,53 @@ import style from './style.css';
 
 const antd = require('antd');
 const Button = antd.Button;
+const Form = antd.Form;
+const Input = antd.Input;
+const Upload = antd.Upload;
+const Icon = antd.Icon;
+const InputNumber = antd.InputNumber;
+const FormItem = Form.Item;
 
-export default class Banners extends Component {
+
+class AddBannerForm extends Component {
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span:4 },
+            wrapperCol: { span: 16 }
+        };
+
+        // upload
+        const uploadProps = {
+            action: '/file/upload',
+            name: 'userfile',
+            // onChange: this.handleChange,
+            multiple: false,
+            fileList: []
+        };
+        return (
+            <Form horizontal>
+                <FormItem {...formItemLayout} label="位置:">
+                    <InputNumber min={1} max={10} defaultValue={1} />
+                </FormItem>
+                <FormItem {...formItemLayout} label="banner图:">
+                    <Upload {...uploadProps} >
+                        <Button type="ghost">
+                            <Icon type="upload" /> 上传
+                        </Button>
+                    </Upload>
+                    <div className={style['image-preview-wrapper']}>图片预览区域</div>
+                </FormItem>
+                <FormItem {...formItemLayout} label="跳转链接:">
+                    <Input placeholder="跳转链接" />
+                </FormItem>
+            </Form>
+        );
+    }
+}
+AddBannerForm = Form.create()(AddBannerForm);
+
+class AddBanner extends Component {
     constructor(props) {
         super(props);
         this.state = {};
@@ -12,7 +57,6 @@ export default class Banners extends Component {
 
     render() {
         let record = this.props.record;
-        debugger;
         return (
             <div>
                 {this.props.record ? '修改banner' : '新建banner'}
@@ -20,3 +64,5 @@ export default class Banners extends Component {
         );
     }
 }
+
+export default AddBannerForm;
