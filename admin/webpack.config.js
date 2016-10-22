0034a8e74c582713b6db686b98e073eb84570812
neo-home-win7 <yyjpig@aliyun.com>
2016-10-22 10:55:45 +0800

    code style

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -28,30 +28,35 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.html$/,
-            loader: 'file?name=[name].[ext]'
-        }, {
-            test: /\.css$/,
-            include: /client/,
-            loaders: [
-                'style-loader',
-                'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]',
-                // 'css-loader',
-                'postcss-loader'
-            ]
-        }, {
-            test: /\.css$/,
-            exclude: /client/,
-            loader: 'style!css'
-        }, {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loaders: [
-                'react-hot',
-                'babel-loader'
-            ]
-        }]
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'file?name=[name].[ext]'
+            },
+            {
+                test: /\.css$/,
+                include: /client/,
+                loaders: [
+                    'style-loader',
+                    'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]',
+                    // 'css-loader',
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /client/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loaders: [
+                    'react-hot',
+                    'babel-loader'
+                ]
+            }
+        ]
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
