d376a9bf8d1ccf93a74fd15220ac0d40a46bc412
neo-home-win7 <yyjpig@aliyun.com>
2017-04-25 23:27:20 +0800

    底部咨询栏

--- frontendIII/public/js/header.js
+++ frontendIII/public/js/header.js
@@ -21,4 +21,30 @@ $('body').on('ready', function () {
     $navs.on('touchend', function (e) {
         e.stopPropagation();
     });
+
+    // 咨询栏
+    (function () {
+        var htmlStr = [
+            '<div class="module-consult-bar">',
+                '<div class="module-consult-bar-main">',
+                    '<div class="module-consult-bar-item module-consult-bar-item-telephone">',
+                        '<a href="#" class="">',
+                            '电话<span class="telephone">：400-850-8368</span><span class="icon-phone"></span>',
+                        '</a>',
+                    '</div>',
+                    '<div class="module-consult-bar-item module-consult-bar-item-consult">',
+                        '<a href="#" class="">在线咨询</a>',
+                    '</div>',
+                    '<div class="module-consult-bar-item module-consult-bar-item-register">',
+                        '<a href="#" class="">冷静报名</a>',
+                    '</div>',
+                    '<div class="module-consult-bar-close"></div>',
+                '</div>',
+            '</div>'
+        ].join('');
+        $('body').append(htmlStr);
+        $('.module-consult-bar-close').on('click', function () {
+            $('.module-consult-bar').remove();
+        });
+    })();
 });
